d0e3213af02a04b9cfce2555d5e07fc02c7b1db4
Adryell William <adrelwilion@gmail.com>
2025-06-07 19:05:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
 
     botao.addEventListener("click", function() {
-        // Esconde o coração
         botao.classList.add("hidden");
-        
-        // Mostra a mensagem com efeito
         setTimeout(function() {
             mensagem.classList.add("visible");
-            body.style.backgroundColor = "#fff9f9";
+            body.style.backgroundImage = "url('fundo.jpg')";
         }, 300);
     });
 });
