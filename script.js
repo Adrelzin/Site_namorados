5cd3c2352daea99108ae43e9d99ef4f7561bea9b
Adryell William <adrelwilion@gmail.com>
2025-06-07 19:15:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const botao = document.getElementById("meubotao");
     const mensagem = document.getElementById("mensagem");
-    const body = document.body;
+    const fundoAnimado = document.getElementById("fundo-animado");
 
     botao.addEventListener("click", function() {
 
         botao.classList.add("hidden");
     
+        fundoAnimado.classList.remove("fundo-escondido");
+        fundoAnimado.classList.add("fundo-visivel");
+        
         setTimeout(function() {
             mensagem.classList.add("visible");
-            body.style.backgroundImage = "url('fundo.jpg')";
-        }, 300);
+        }, 200);
     });
 });
